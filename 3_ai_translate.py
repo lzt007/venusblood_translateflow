b28bb7b9ead1bdd5527c90488f12fda708fef7aa
lzt007 <lizitian007007@gmail.com>
2025-08-14 20:02:34 +0800

    1.调整项目结构 2.添加剧情选择支翻译

--- 3_ai_translate.py
+++ 3_ai_translate.py
@@ -1,64 +1,9 @@
-import os
-import json
-import traceback
-# from translator import translate
-# from translator_manual import translate
-from translator_sakurallm import translate
+from scenario.translate import run
 
 keyjson_dir = "./keyjson"
 translated_dir = "./translated"
 roles_path = "./static/roles.json"
 subTitles_path = "./static/subTitles.json"
+sub_role = "罗兰"
 
-with open(roles_path, "r", encoding="utf-8") as f:
-    roles: dict = json.load(f)
-
-with open(subTitles_path, "r", encoding="utf-8") as f:
-    subTitles: dict = json.load(f)
-
-for root, dirs, files in os.walk(keyjson_dir):
-    for file in files:
-        try:
-            with open(os.path.join(root, file), "r", encoding="utf-8") as f:
-                data_list = json.load(f)
-            total = len(data_list)
-            print("正在构造术语表")
-            # 构造人名和章节名术语表
-            for i in range(0, len(data_list)):
-                print(f"{i+1}/{total}")
-                if data_list[i]["subTitle"] in subTitles:
-                    subtitle = subTitles[data_list[i]["subTitle"]]
-                else:
-                    subtitle = translate(data_list[i]["subTitle"])
-                    subTitles[data_list[i]["subTitle"]] = subtitle
-                    with open(subTitles_path, "w", encoding="utf-8") as f:
-                        f.write(json.dumps(subTitles, ensure_ascii=False))
-                data_list[i]["subTitle"] = subtitle
-                if data_list[i]["window"] is None:
-                    continue
-                if data_list[i]["window"]["tag"] != "" and data_list[i]["window"]["name"] == "":
-                    # 实际游戏中应当存在某个数据表根据tag查名字，tag不是用于显示的值，不要修改tag！！！
-                    # name在不为空时会覆盖显示名字，原本用于给初次登场的角色显示？？？用，这里利用这个键值覆写中文名
-                    if data_list[i]["window"]["tag"] in roles:
-                        name = roles[data_list[i]["window"]["tag"]]
-                    else:
-                        name = translate(data_list[i]["window"]["tag"])
-                        roles[data_list[i]["window"]["tag"]] = name
-                        with open(roles_path, "w", encoding="utf-8") as f:
-                            f.write(json.dumps(roles, ensure_ascii=False))
-                    data_list[i]["window"]["name"] = name
-            print("正在翻译对话")
-            # 翻译剧本
-            for i in range(0, len(data_list)):
-                print(f"{i+1}/{total}")
-                if data_list[i]["window"] is None:
-                    continue
-                for j in range(0, len(data_list[i]["window"]["texts"])):
-                    data_list[i]["window"]["texts"][j] = translate(data_list[i]["window"]["texts"][j],{},role=data_list[i]["window"]["name"])
-                print(data_list[i])
-            with open(os.path.join(translated_dir, file), "w", encoding="utf-8") as f:
-                f.write(json.dumps(data_list, ensure_ascii=False, indent=2))
-
-
-        except BaseException as e:
-            print(f"解析{file}时发生错误：{e}\n{traceback.format_exc()}")
+run(keyjson_dir, translated_dir, roles_path, subTitles_path, sub_role)
